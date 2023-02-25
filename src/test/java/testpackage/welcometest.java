e91b0f60b7dd0b153393c1be0b986daa29c6b0cd
saipavana <arumillisaipavan1998@gmail.com>
2023-02-25 19:39:07 +0530

    create testgitproject1

---
+++ src/test/java/testpackage/welcometest.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+public class welcometest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("welcome in hugelearning.com");
+	}
+
+}
